0322048193cc1532cd101935607e95eaf65d214e
Flora PREUVOT <flora.preuvot2025@campus-eni.fr>
2026-04-07 13:35:57 +0200

    setup

---
+++ src/test/java/fr/eni/bookhubbackend/BookhubBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.eni.bookhubbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookhubBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
