a72b5a5c34fe209ba9b642ebc0ec4406a8f4e569
rmontassier2023 <romain.montassier2023@campus-eni.fr>
2026-04-10 12:09:01 +0200

    searchBooks

--- src/main/java/fr/eni/bookhubbackend/controller/BookController.java
+++ src/main/java/fr/eni/bookhubbackend/controller/BookController.java
@@ -1,13 +1,14 @@
 package fr.eni.bookhubbackend.controller;
 
 import fr.eni.bookhubbackend.entity.dto.BookDto;
+import fr.eni.bookhubbackend.entity.dto.Search;
 import fr.eni.bookhubbackend.service.BookService;
 import lombok.AllArgsConstructor;
+import org.springdoc.core.annotations.ParameterObject;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @AllArgsConstructor
 @RestController
@@ -20,5 +21,15 @@ public class BookController {
     @GetMapping("{id}")
     public ResponseEntity<BookDto> findBookById(@PathVariable final Long id) {
         return ResponseEntity.ok(bookService.findBookById(id));
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<BookDto>> findAllBooks(final Pageable pageable) {
+        return ResponseEntity.ok(bookService.findAllBooks(pageable));
+    }
+
+    @GetMapping("search")
+    public ResponseEntity<Page<BookDto>> searchBooks(@ParameterObject Search search, final Pageable pageable) {
+        return ResponseEntity.ok(bookService.searchBooks(search, pageable));
     }
 }
--- src/main/java/fr/eni/bookhubbackend/service/BookService.java
+++ src/main/java/fr/eni/bookhubbackend/service/BookService.java
@@ -1,9 +1,12 @@
 package fr.eni.bookhubbackend.service;
 
 import fr.eni.bookhubbackend.entity.dto.BookDto;
+import fr.eni.bookhubbackend.entity.dto.Search;
 import fr.eni.bookhubbackend.mapper.BookMapper;
 import fr.eni.bookhubbackend.repository.BookRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
@@ -21,5 +24,13 @@ public class BookService {
     public BookDto findBookById(final Long id) {
 
         return bookMapper.toBookDto(bookRepository.findById(id).orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, BOOK_NOT_FOUND)));
+    }
+
+    public Page<BookDto> findAllBooks(final Pageable pageable) {
+        return bookRepository.findAll(pageable).map(bookMapper::toBookDto);
+    }
+
+    public Page<BookDto> searchBooks(final Search search, final Pageable pageable) {
+        return bookRepository.searchBook(search, pageable).map(bookMapper::toBookDto);
     }
 }
